bdb0f51059eb83a28269eadda8faded0055e4ab3
Yashwardhan Pauranik <yash@appointy.com>
2018-05-16 07:54:33 +0530

    post user login

--- server/models/user.js
+++ server/models/user.js
@@ -57,6 +57,28 @@ UserSchema.statics.findByToken = function(token) {
     });
 };
 
+UserSchema.statics.findByCredentials = function(email, password) {
+    let user = this;
+    
+    return User.findOne({email})
+        .then((user) => {
+            if (!user) {
+                return Promise.reject();
+            }
+
+            return new Promise((resolve, reject) => {
+                bcrypt.compare(password, user.password, (err, res) => {
+                    if (res) {
+                        resolve(user);                        
+                    } else {
+                        reject();
+                    }
+                });
+            })
+        })
+        .catch((e) => Promise.reject());
+};
+
 UserSchema.methods.generateAuthToken = function() {
     let user = this;
     let access = 'auth';
--- server/server.js
+++ server/server.js
@@ -105,6 +105,18 @@ app.post('/users', (req, res) => {
     });
 });
 
+app.post('/users/login', (req, res) => {
+    let user = _.pick(req.body, ['email', 'password']);
+
+    User.findByCredentials(user.email, user.password)
+        .then((user) => {
+            return user.generateAuthToken().then((token) => {
+                res.header('x-auth', token).send(user)
+            })
+        })
+        .catch((err) => res.status(400).send());
+});
+
 app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
 });
